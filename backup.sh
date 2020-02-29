b842f1860df99563a83afd40b6da42e42e636ef1
Erlend Klakegg Bergheim <erlend@klakegg.net>
2020-02-29 17:47:20 +0100

    Some more refactoring.

--- backup.sh
+++ backup.sh
@@ -1,26 +1,41 @@
 #!/bin/sh
 
+# Main
+
 gdrive-backup \
     "1T6z2NZ4mc69YllZOXE5TnT5Ey-FlVtaXN1oQ4AIMp7g" \
     "application/vnd.oasis.opendocument.spreadsheet" \
-    "backup/Signature.ods"
+    "src/main/Signature.ods"
 
 gdrive-backup \
     "1XjhMEKJLpKfn1Z6bjNVT5myoYvsvD2yQQsnRja1QGq0" \
     "application/vnd.oasis.opendocument.spreadsheet" \
-    "backup/Library.ods"
+    "src/main/Library.ods"
 
 gdrive-backup \
     "1vsjDwzxmXbeJU11mu8wPQIDIP3I0g2GlQP9ObjhKCJ0" \
     "application/vnd.oasis.opendocument.spreadsheet" \
-    "backup/Documents.ods"
+    "src/main/Documents.ods"
 
 gdrive-backup \
     "1ra0tRhlSixWSnM5fEf-jUURGpJLIUO2Y7zZ2OY2H6GE" \
     "text/plain" \
-    "backup/party-summary-information.xml"
+    "src/main/party-summary-information.xml"
 
 gdrive-backup \
     "16jacpr-68Z_FCJmmrUVunarYkU4Qz10PI_uBOiB6Lv8" \
     "text/plain" \
-    "backup/schema-summary-information.xml"
+    "src/main/schema-summary-information.xml"
+
+
+# Transportation (tsc)
+
+gdrive-backup \
+    "16MPInQ2ubZSTvtZWBqOgtaqNcuNHxW8aNrG2HGrH4yI" \
+    "application/vnd.oasis.opendocument.spreadsheet" \
+    "src/tsc/Library.ods"
+
+gdrive-backup \
+    "1j6sHdX9HX8VQ7mAr4DFbOchNHfF9g0HtC6z3Y43tvM4" \
+    "application/vnd.oasis.opendocument.spreadsheet" \
+    "src/tsc/Documents.ods"
