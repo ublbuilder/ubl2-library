be306812de8364c068df4943812c411a350341ab
Erlend Klakegg Bergheim <erlend@klakegg.net>
2020-02-29 11:47:50 +0100

    Changing backup folder.

--- backup.sh
+++ backup.sh
@@ -3,24 +3,24 @@
 gdrive-backup \
     "1T6z2NZ4mc69YllZOXE5TnT5Ey-FlVtaXN1oQ4AIMp7g" \
     "application/vnd.oasis.opendocument.spreadsheet" \
-    "google/Signature.ods"
+    "backup/Signature.ods"
 
 gdrive-backup \
     "1XjhMEKJLpKfn1Z6bjNVT5myoYvsvD2yQQsnRja1QGq0" \
     "application/vnd.oasis.opendocument.spreadsheet" \
-    "google/Library.ods"
+    "backup/Library.ods"
 
 gdrive-backup \
     "1vsjDwzxmXbeJU11mu8wPQIDIP3I0g2GlQP9ObjhKCJ0" \
     "application/vnd.oasis.opendocument.spreadsheet" \
-    "google/Documents.ods"
+    "backup/Documents.ods"
 
 gdrive-backup \
     "1ra0tRhlSixWSnM5fEf-jUURGpJLIUO2Y7zZ2OY2H6GE" \
     "text/plain" \
-    "google/party-summary-information.xml"
+    "backup/party-summary-information.xml"
 
 gdrive-backup \
     "16jacpr-68Z_FCJmmrUVunarYkU4Qz10PI_uBOiB6Lv8" \
     "text/plain" \
-    "google/schema-summary-information.xml"
+    "backup/schema-summary-information.xml"
